9770092f453028a09f50bebf3cdf509056637d20
sanket-burde <39940573+sanket-burde@users.noreply.github.com>
2025-08-11 19:03:37 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,3 +1,6 @@
 def call(string url, string branch){
-git url:"${url}", branch:"${branch}"
+   echo 'clone url'
+   git url:"${url}", branch:"${branch}"             
+   echo "code cloning succefully"
+
 }
