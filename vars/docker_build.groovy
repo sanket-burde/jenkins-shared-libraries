e3496e7acb3835ac8360c721f6a19b8393c553e9
sanket-burde <39940573+sanket-burde@users.noreply.github.com>
2025-08-11 19:24:25 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(string ProjectName, string ImageTag, string DockerHubUser){
-sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String projectName, String imageTag, String dockerHubUser) {
+    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
 }
